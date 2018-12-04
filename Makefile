9f3e1038bbdff62734b110f42ca344377e77aba1
Michael <ma16064@my.bristol.ac.uk>
2018-12-04 13:35:26 +0000

    first

--- Makefile
+++ Makefile
@@ -1,6 +1,2 @@
 stencil: stencil.c
-	icc -std=c99 -Wall $^ -o $@
-gpr: stencil.c
-		icc -std=c99 -pg -fopenmp -g -o stream.gprof stencil.c
-		./stream.gprof 1024 1024 100
-	  gprof -b -l stream.gprof gmon.out
+	icc -std=c99 -Wall -xAVX $^ -o $@
--- stencil.c
+++ stencil.c
@@ -6,65 +6,89 @@
 // Define output file name
 #define OUTPUT_FILE "stencil.pgm"
 
-void stencil(const int nx, const int ny, double *  image, double *  tmp_image);
-void init_image(const int nx, const int ny, double *  image, double *  tmp_image);
-void output_image(const char * file_name, const int nx, const int ny, double *image);
+void stencil(const int nx, const int ny, float *  image, float *  tmp_image);
+void init_image(const int nx, const int ny, float *  image, float *  tmp_image);
+void output_image(const char * file_name, const int nx, const int ny, float *image);
 double wtime(void);
 
-int main(int argc, char *argv[]) {
 
-  // Check usage
-  if (argc != 4) {
-    fprintf(stderr, "Usage: %s nx ny niters\n", argv[0]);
-    exit(EXIT_FAILURE);
+void stencil(const int nx, const int ny, float * restrict image, float * restrict tmp_image) {
+  // top left
+  tmp_image[0] = image[0] * (float) 0.6;
+  tmp_image[0] += image[ny] * (float) 0.1;
+  tmp_image[0] += image[1] * (float) 0.1;
+
+  // top right
+  int x = ny - 1;
+  tmp_image[x] = image[x] * (float) 0.6;
+  tmp_image[x] += image[x+ny] * (float) 0.1;
+  tmp_image[x] += image[x-1] * (float) 0.1;
+
+  // bottom left
+  x = nx*(ny-1);
+  tmp_image[x] = image[x] * (float) 0.6;
+  tmp_image[x] += image[x-ny] * (float) 0.1;
+  tmp_image[x] += image[x+1] * (float) 0.1;
+
+  // bottom right
+  x = ny*nx - 1;
+  tmp_image[x] = image[x] * (float) 0.6;
+  tmp_image[x] += image[x-ny] * (float) 0.1;
+  tmp_image[x] += image[x-1] * (float) 0.1;
+
+  // top edge
+  for (int k = 1; k < ny-1; k++)
+  {
+    tmp_image[k] = image[k] * (float) 0.6;
+    tmp_image[k] += image[k+ny] * (float) 0.1;
+    tmp_image[k] += image[k-1] * (float) 0.1;
+    tmp_image[k] += image[k+1] * (float) 0.1;
   }
 
-  // Initiliase problem dimensions from command line arguments
-  int nx = atoi(argv[1]);
-  int ny = atoi(argv[2]);
-  int niters = atoi(argv[3]);
-
-  // Allocate the image
-  double *image = malloc(sizeof(double)*nx*ny);
-  double *tmp_image = malloc(sizeof(double)*nx*ny);
-
-  // Set the input image
-  init_image(nx, ny, image, tmp_image);
-
-  // Call the stencil kernel
-  double tic = wtime();
-  for (int t = 0; t < niters; ++t) {
-    stencil(nx, ny, image, tmp_image);
-    stencil(nx, ny, tmp_image, image);
+  // bottom edge
+  for (int k = nx*(ny-1)+1; k < ny*nx-1; k++)
+  {
+    tmp_image[k] = image[k] * (float) 0.6;
+    tmp_image[k] += image[k-ny] * (float) 0.1;
+    tmp_image[k] += image[k-1] * (float) 0.1;
+    tmp_image[k] += image[k+1] * (float) 0.1;
   }
-  double toc = wtime();
 
+  // left edge
+  for (int k = ny; k < nx*(ny-1); k += ny)
+  {
+    tmp_image[k] = image[k] * (float) 0.6;
+    tmp_image[k] += image[k-ny] * (float) 0.1;
+    tmp_image[k] += image[k+ny] * (float) 0.1;
+    tmp_image[k] += image[k+1] * (float) 0.1;
+  }
 
-  // Output
-  printf("------------------------------------\n");
-  printf(" runtime: %lf s\n", toc-tic);
-  printf("------------------------------------\n");
+  // right edge
+  for (int k = 2*ny-1; k < nx*ny-1; k += ny)
+  {
+    tmp_image[k] = image[k] * (float) 0.6;
+    tmp_image[k] += image[k-ny] * (float) 0.1;
+    tmp_image[k] += image[k+ny] * (float) 0.1;
+    tmp_image[k] += image[k-1] * (float) 0.1;
+  }
 
-  output_image(OUTPUT_FILE, nx, ny, image);
-  free(image);
-}
-
-void stencil(const int nx, const int ny, double *  image, double *  tmp_image) {
-
-  for (int j = 0; j < ny; j++) {
-    for (int i = 0; i < nx; i++) {
-      int x = j+i*ny;
-      tmp_image[x] = image[x] * 0.6;
-      if (i > 0) tmp_image[x] += image[j+(i-1)*ny] * 0.1;
-      if (i < nx-1) tmp_image[x] += image[j+(i+1)*ny] * 0.1;
-      if (j > 0) tmp_image[x] += image[x-1] * 0.1;
-      if (j < ny-1) tmp_image[x] += image[x+1] * 0.1;
+  // main
+  for (int rowNumber = 1; rowNumber < nx-1; rowNumber++)
+  {
+    for (int x = 1; x < ny-1; x++)
+    {
+      int k = rowNumber*ny + x;
+      tmp_image[k] = image[k] * (float) 0.6;
+      tmp_image[k] += image[k-ny] * (float) 0.1;
+      tmp_image[k] += image[k+ny] * (float) 0.1;
+      tmp_image[k] += image[k-1] * (float) 0.1;
+      tmp_image[k] += image[k+1] * (float) 0.1;
     }
   }
 }
 
 // Create the input image
-void init_image(const int nx, const int ny, double *  image, double *  tmp_image) {
+void init_image(const int nx, const int ny, float *  image, float *  tmp_image) {
   // Zero everything
   for (int j = 0; j < ny; ++j) {
     for (int i = 0; i < nx; ++i) {
@@ -87,7 +111,7 @@ void init_image(const int nx, const int ny, double *  image, double *  tmp_image
 }
 
 // Routine to output the image in Netpbm grayscale binary image format
-void output_image(const char * file_name, const int nx, const int ny, double *image) {
+void output_image(const char * file_name, const int nx, const int ny, float *image) {
 
   // Open output file
   FILE *fp = fopen(file_name, "w");
@@ -102,7 +126,7 @@ void output_image(const char * file_name, const int nx, const int ny, double *im
   // Calculate maximum value of image
   // This is used to rescale the values
   // to a range of 0-255 for output
-  double maximum = 0.0;
+  float maximum = 0.0;
   for (int j = 0; j < ny; ++j) {
     for (int i = 0; i < nx; ++i) {
       if (image[j+i*ny] > maximum)
@@ -127,4 +151,42 @@ double wtime(void) {
   struct timeval tv;
   gettimeofday(&tv, NULL);
   return tv.tv_sec + tv.tv_usec*1e-6;
+}
+
+int main(int argc, char *argv[]) {
+
+  // Check usage
+  if (argc != 4) {
+    fprintf(stderr, "Usage: %s nx ny niters\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  // Initiliase problem dimensions from command line arguments
+  int nx = atoi(argv[1]);
+  int ny = atoi(argv[2]);
+  int niters = atoi(argv[3]);
+
+  // Allocate the image
+  float *image = malloc(sizeof(float)*nx*ny);
+  float *tmp_image = malloc(sizeof(float)*nx*ny);
+
+  // Set the input image
+  init_image(nx, ny, image, tmp_image);
+
+  // Call the stencil kernel
+  double tic = wtime();
+  for (int t = 0; t < niters; ++t) {
+    stencil(nx, ny, image, tmp_image);
+    stencil(nx, ny, tmp_image, image);
+  }
+  double toc = wtime();
+
+
+  // Output
+  printf("------------------------------------\n");
+  printf(" runtime: %lf s\n", toc-tic);
+  printf("------------------------------------\n");
+
+  output_image(OUTPUT_FILE, nx, ny, image);
+  free(image);
 }
